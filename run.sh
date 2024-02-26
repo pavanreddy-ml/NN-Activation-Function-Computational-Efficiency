37d60f96f749f432c5935748bb26f70caa9e2d61
Pavan Reddy <86465783+pavanreddy-ml@users.noreply.github.com>
2024-02-26 11:12:15 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Replace the URL below with your repository URL
 REPO_URL="https://github.com/pavanreddy-ml/NN-Activation-Function-Computational-Efficiency.git"
-REPO_DIR="Sample_Capstone\code"
+REPO_DIR="NN-Activation-Function-Computational-Efficiency/Sample_Capstone/code/"
 
 # Clone Git repository
 git clone $REPO_URL
@@ -10,11 +10,13 @@ cd $REPO_DIR
 
 # Install Python and pip
 sudo apt-get update
-sudo apt-get install -y python3
-sudo apt-get install -y python3-pip
+sudo apt-get install -yq python3 python3-pip
 
 # Install required Python packages
-pip3 install -r requirements.txt
+pip install numpy
+pip install pandas
+pip install psutil
+pip install scikit-learn
 pip install tensorflow
 
 # Run main.py script
