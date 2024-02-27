42d0ea0ddc4dd2c36dc73cd6c3d6307b79f471f7
Pavan Reddy <86465783+pavanreddy-ml@users.noreply.github.com>
2024-02-26 23:57:10 -0500

    Add files via upload

---
+++ Sample_Capstone/code/config.py
@@ -0,0 +1,11 @@
+RESULTS_PATH = "results.csv"
+RUNS = 200
+VERBOSE = True
+OVERWRITE = False
+CAHCE = False
+NEW_RESULTS_FILE = False
+EPOCHS = 100
+BATCH_SIZE = 1024
+
+COLS = ["dataset", "activation", "run", "continuous_forward_profile_time", "continuous_cpu_time", "continuous_performance_metric",
+        "piecewise_forward_profile_time", "piecewise_cpu_time", "piecewise_performance_metric"]
