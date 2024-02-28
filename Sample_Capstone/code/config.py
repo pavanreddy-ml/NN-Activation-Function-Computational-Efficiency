ad4f604e10a5a113912fe763fdea341de6457c96
Pavan Reddy <86465783+pavanreddy-ml@users.noreply.github.com>
2024-02-27 20:15:36 -0500

    Add files via upload

---
+++ Sample_Capstone/code/config.py
@@ -0,0 +1,12 @@
+RESULTS_PATH = "results.csv"
+RUNS = 200
+VERBOSE = True
+OVERWRITE = False
+CACHE = False
+NEW_RESULTS_FILE = False
+EPOCHS = 10
+BATCH_SIZE = 1024
+
+COLS = ["dataset", "activation", "run", "continuous_activation_time", "continuous_train_time", "continuous_performance_metric",
+        "piecewise_activation_time", "piecewise_train_time", "piecewise_performance_metric", "piecewise_profile_data",
+        "combined_activation_time", "combined_train_time", "combined_performance_metric", "combined_profile_data"]
